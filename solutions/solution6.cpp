09b1fe7463e5f14ba8b849ae8374d8d1c368b12f
Adam Gatt <adam.gatt@live.com.au>
2024-12-06 23:33:32 +1030

    Complete Day 6 Part B

--- solutions/solution6.cpp
+++ solutions/solution6.cpp
@@ -1,43 +1,61 @@
 #include "solutions.h"
 #include "solution6.h"
 
+#include <functional>
 #include <numeric>
 
 auto Solutions::solution6() -> Answers {
 
-    const auto original_map = Utils::readGrid<130, 130>("inputs/input6.txt");
+    const auto original_map = Utils::readGrid<rows, cols>("inputs/input6.txt");
     Location start_location = find_start(original_map);
 
     // Initial walk of the original map for Part A with no map modifications.
     // Result of walk_map() is ignored as we assume no infinite loop will
     // occur with the original map.
     auto map = original_map; // Make copy as walk_map modifies the input
-    walk_map(map, start_location); 
-    int answerA = count_walked(map);
+    const auto& [_, visitedMap] = walk_map(map, start_location); 
+    int answerA = count_walked(visitedMap);
 
     // For Part B we iterate through all empty map locations, insert a single
     // wall there and walk the map to determine if an infinite loop will occur
     int answerB = 0;
-    // for (int r = 0; r < rows; ++r) {
-    //     for (int c = 0; c < cols; ++c) {
-    //         // Found an originally-empty location to try a wall
-    //         if (original_map[r][c] == sym_empty) {
-    //             map = original_map;
-    //             map[r][c] = sym_wall;
-    //             if (walk_map(map, start_location) == WalkResult::Loop) {
-    //                 answerB++;
-    //             }
-    //         }
-    //     }
-    // }
+    for (int r = 0; r < rows; ++r) {
+        for (int c = 0; c < cols; ++c) {
+            // Found an originally-empty location to try a wall
+            if (original_map[r][c] == sym_empty) {
+                // Clone the map and add that extra wall
+                map = original_map;
+                map[r][c] = sym_wall;
+                // Walk the modified map and determine if a loop occurred
+                const auto& [walkResult, _] = walk_map(map, start_location); 
+                if (walkResult == WalkResult::Loop) {
+                    answerB++;
+                }
+            }
+        }
+    }
 
     return {std::to_string(answerA), std::to_string(answerB)};
 }
 
-auto turn_right(const DirectionItr& direction) -> DirectionItr {
-    DirectionItr out = std::next(direction);
-    
-    return out != directions.end() ? out : directions.begin();
+auto turn_right(Direction direction) -> std::pair<Direction, StepOffset> {
+    Direction newDirection;
+    switch (direction) {
+        case Direction::Up:
+            newDirection = Direction::Right;
+            break;
+        case Direction::Right:
+            newDirection = Direction::Down;
+            break;
+        case Direction::Down:
+            newDirection = Direction::Left;
+            break;
+        case Direction::Left:
+            newDirection = Direction::Up;
+            break;
+    }
+
+    return {newDirection, step_offsets[static_cast<int>(newDirection)]};
 }
 
 auto find_start(const Map& map) -> Location {
@@ -57,57 +75,61 @@ auto outside_map(const Location& location) -> bool {
         || location.second >= cols;
 }
 
-auto mark_walked(Map& map, const Location& location) -> void {
-    map[location.first][location.second] = sym_walked;
+auto mark_walked(VisitedMap& visitedMap, const Location& location, Direction direction) -> void {
+    visitedMap[location.first][location.second][static_cast<int>(direction)] = true;
 }
 
-auto walk_map(Map& map, const Location& start_location) -> WalkResult {
+auto walk_map(const Map& map, const Location& start_location) -> std::pair<WalkResult, VisitedMap> {
     // Begin at starting location facing upwards
     Location location = start_location;
-    DirectionItr direction = directions.begin();
+    Direction direction = Direction::Up;
+    StepOffset step_offset = step_offsets[static_cast<int>(direction)];
+    VisitedMap visitedMap {};
 
-    mark_walked(map, location);
+    mark_walked(visitedMap, location, direction);
 
     while (true) {
         Location next_step = {
-            location.first + direction->first,
-            location.second + direction->second
+            location.first + step_offset.first,
+            location.second + step_offset.second
         };
 
-        // Check if left map
+        // Check if next step is outside map
         if (outside_map(next_step)) {
-            return WalkResult::LeftMap;
-        } else if (next_step == start_location && direction == directions.begin()) {
-            return WalkResult::Loop;
+            return {WalkResult::LeftMap, visitedMap};
+        } else if (visitedMap[next_step.first][next_step.second][static_cast<int>(direction)]) {
+            // Check if we are re-visiting the same tile and with the same direction too
+            return {WalkResult::Loop, visitedMap};
         }
 
-        // Check if in infinite loop by determining if the
-        // next step will place the path back at the start
-        // location and facing upwards
-
+        // Otherwise we are fine to continue walking, so either advance or
+        // turn depending on the tile we are about to enter
         switch (map[next_step.first][next_step.second]) {
-            case sym_empty:
-            case sym_start: // Fallthrough
-            case sym_walked: // Fallthrough
-                mark_walked(map, next_step);
+            case sym_empty: // Fallthrough
+            case sym_start:
                 location = next_step;
+                mark_walked(visitedMap, location, direction);
                 break;
             case sym_wall:
-                direction = turn_right(direction);
+                auto turnResult = turn_right(direction);
+                direction = turnResult.first;
+                step_offset = turnResult.second;
                 break;
         }
     }
 }
 
-auto count_walked(const Map& map) -> size_t {
-    return std::transform_reduce(
-        map.begin(), map.end(),
-        0, std::plus{},
-        [](const Utils::Row<cols>& col){return std::ranges::count(col, sym_walked);}
-    );
-}
+auto count_walked(const VisitedMap& visitedMap) -> size_t {
+    size_t walked = 0;
 
-auto operator==(const Location& lhs, const Location& rhs) -> bool {
-    return lhs.first == rhs.first
-        && lhs.second == rhs.second;
+    for (int r = 0; r < rows; ++r) {
+        for (int c = 0; c < cols; ++c) {
+            // Count the tile if it was walked over from any direction
+            if (std::ranges::any_of(visitedMap[r][c], std::identity{})) {
+                walked++;
+            }
+        }
+    }
+
+    return walked;
 }
--- solutions/solution6.h
+++ solutions/solution6.h
@@ -8,38 +8,41 @@
 
 constexpr int rows = 130;
 constexpr int cols = 130;
-constexpr char sym_walked = '*';
 constexpr char sym_empty = '.';
 constexpr char sym_wall = '#';
 constexpr char sym_start = '^';
 
 using Map = Utils::Grid<rows, cols>;
 using Location = std::pair<size_t, size_t>;
-using Direction = std::pair<int, int>;
+using VisitedMap = std::array<std::array<std::array<bool, 4>, cols>,rows>;
 
-constexpr std::array<Direction, 4> directions {{
+enum class Direction {
+    Up = 0,
+    Right = 1,
+    Down = 2,
+    Left = 3
+};
+
+using StepOffset = std::pair<int, int>;
+constexpr std::array<StepOffset, 4> step_offsets {{
     {-1,  0}, // Walk up (starting direction)
     { 0,  1}, // Walk right
     { 1,  0}, // Walk down
     { 0, -1}  // Walk left
 }};
 
-using DirectionItr = std::array<Direction, 4>::const_iterator;
-
 enum class WalkResult {LeftMap, Loop};
 
-auto turn_right(const DirectionItr& direction) -> DirectionItr;
+auto turn_right(Direction direction) -> std::pair<Direction, StepOffset>;
 
 auto find_start(const Map& map) -> Location;
 
 auto outside_map(const Location& location) -> bool;
 
-auto mark_walked(Map& map, const Location& location) -> void;
+auto mark_walked(VisitedMap& visitedMap, const Location& location, Direction direction) -> void;
 
-auto walk_map(Map& map, const Location& start_location) -> WalkResult;
+auto walk_map(const Map& map, const Location& start_location) -> std::pair<WalkResult, VisitedMap>;
 
-auto count_walked(const Map& map) -> size_t;
-
-auto operator==(const Location& lhs, const Location& rhs) -> bool;
+auto count_walked(const VisitedMap& visitedMap) -> size_t;
 
 #endif
